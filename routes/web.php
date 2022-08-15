08a9c9c0e6aaa4a0454013834e6763fe624c2d9b
Susant Paudel <techbizznepal@gmail.com>
2022-08-15 20:45:06 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Imports\DataImport;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-use Maatwebsite\Excel\Excel;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
@@ -14,16 +11,4 @@ Route::get('/gallery', [HomeController::class, 'gallery'])->name('gallery');
 Route::get('/reservation', [HomeController::class, 'reservation'])->name('reservation');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 
-Route::get('/import', function (Excel $excel) {
-    $filePath = public_path('data.xlsx');
-//    return $filePath;
-    $excel->import(new DataImport('menus'), $filePath);
-    $excel->import(new DataImport('items'), $filePath);
-    return 'yes';
-});
-Route::get('mail', function () {
-    Mail::raw('Text to e-mail', function ($message) {
-        $message->to('gracysusant@gmail.com');
-    });
-});
 Auth::routes();
