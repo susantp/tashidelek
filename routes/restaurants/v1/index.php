fa8183b336f02315f1c09bdd01ab54609b49eadb
Susant Paudel <techbizznepal@gmail.com>
2022-08-16 04:17:28 +0545

    Update index.php

--- routes/restaurants/v1/index.php
+++ routes/restaurants/v1/index.php
@@ -5,40 +5,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     $title = config('app.name');
     $info = [
+        'logoFile' => 'logo.png',
         'title' => $title,
         'description' => 'Our Chefs offer you perfect cooking, best-served dishes with fresh ingredients and old recipes. We have only carefully'.'</br>'.'sourced and seasonal ingredients at our disposal to make rustic dishes. We provide you with daily Mo: Mo, Tandoori,'.'</br>'.'Vegetable Korma and many more.',
         'sliders' => [
             [
-                'subTitle' => '',
-                'title' => '',
-                'description' => ''
+                'position' => 0,
+                'id' => 'slider1',
+                'subTitle' => 'Your Favourite',
+                'title' => 'TASHI DELEK',
+                'description' => 'Welcome to Tashi Delek, a place where you can order your favorite restaurant dishes, drinks, and desserts at affordable price. We are glad to offer you the best food in the area to everyone.'
             ],
             [
-                'subTitle' => '',
-                'title' => '',
-                'description' => ''
-            ],
-            [
-                'subTitle' => '',
-                'title' => '',
-                'description' => ''
-            ],
+                'position' => 1,
+                'id' => 'slider2',
+                'subTitle' => 'Affordable Organic Food',
+                'title' => 'HEALTHY BREAKFASTS',
+                'description' => 'Looking for fresh and tasty breakfasts and lunches? At Tashi Delek, you can order any kind of morning snack from local restaurants at reasonable prices without any extra charges.'
+            ]
         ],
         'pages' => [
             [
                 'title' => 'Our Menu',
                 'slug' => 'menu',
-                'image' => ''
+                'image' => 'https://via.placeholder.com/200.png'
             ],
             [
                 'title' => 'Catering',
                 'slug' => 'catering',
-                'image' => ''
+                'image' => 'https://via.placeholder.com/200.png'
             ],
             [
                 'title' => 'Events',
                 'slug' => 'events',
-                'image' => ''
+                'image' => 'https://via.placeholder.com/200.png'
             ]
         ]
     ];
